c579ab198a8c5f0b4cad71e7266e27e61490448b
darklordnorge <romteknolog@gmail.com>
2014-04-09 20:37:11 +0100

    Fixed compilation bugs and removed unneeded files

--- lib/reference_points.c
+++ lib/reference_points.c
@@ -5,9 +5,11 @@
  * Author:        Stefan Klaus
  * Modifications: V 0.1
  */
+
 #include "reference_points.h"
 #include "odometry.h"
 #include "e_puck_movement.h"
+#include "map_building.h"
 
  /**
 Sets the reference point in the referencePos struct.
@@ -52,30 +54,30 @@ void updateReferencePoints(struct odometryTrackStruct * ot, struct referencePos 
 	double dThreshold = 10.0;
 	double *point_dEncPos;
 	double dCurPosX = ot->result.x;
+	int direction = check_direction(ot->result.theta);
 
-	check_direction(ot->result.theta);
 	point_dEncPos = get_encoder_positions();
 
-	if(north){
+	if(direction == 1){
 		if(((ref->upper_left.x <= dCurPosX + dThreshold) || (ref->upper_left.x >= dCurPosX - dThreshold)) && corner == 3){
 			setReferencePoint(ref, corner);
 		}else if(((ref->upper_right.x <= dCurPosX + dThreshold )|| (ref->upper_right.x >= dCurPosX - dThreshold)) && corner == 4){
 			setReferencePoint(ref, corner);
 		}
-	}else if(east){
+	}else if(direction == 2){
 		if(((ref->upper_right.x <= dCurPosX + dThreshold) || (ref->upper_right.x >= dCurPosX - dThreshold)) && corner == 4){
 			setReferencePoint(ref, corner);
 		}else if(((ref->lower_right.x <= dCurPosX + dThreshold) || (ref->lower_right.x >= dCurPosX - dThreshold)) && corner == 2){
 			setReferencePoint(ref, corner);
 		}
-	}else if(south){
+	}else if(direction == 3){
 		if(((ref->lower_left.x <= dCurPosX + dThreshold) || (ref->lower_left.x >= dCurPosX - dThreshold)) && corner == 1){
 			ref->lower_left.x += point_dEncPos[0];
 			ref->lower_left.y += point_dEncPos[1];
 		}else if(((ref->lower_right.x <= dCurPosX + dThreshold) || (ref->lower_right.x >= dCurPosX - dThreshold)) && corner == 2){
 			setReferencePoint(ref, corner);
 		}
-	}else if(west){
+	}else if(direction == 4){
 		if(((ref->upper_left.x <= dCurPosX + dThreshold) || (ref->upper_left.x >= dCurPosX - dThreshold)) && corner == 3){
 			setReferencePoint(ref, corner);
 		}else if(((ref->lower_right.x <= dCurPosX + dThreshold) || (ref->lower_right.x >= dCurPosX - dThreshold)) && corner == 1){
@@ -84,7 +86,7 @@ void updateReferencePoints(struct odometryTrackStruct * ot, struct referencePos 
 	}
 }
 
-void checkReferencePoints(odometryTrackStruct * ot, struct referencePos * ref, int corner){
+void checkReferencePoints(struct odometryTrackStruct * ot, struct referencePos * ref, int corner){
 	double dThreshold = 10.0f;
 }
 
