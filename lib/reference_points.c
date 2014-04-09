50ab3708b0706dd54c67458855c11f1cd9dedc4b
darklordnorge <romteknolog@gmail.com>
2014-04-09 17:09:09 +0100

    Created refrence_point.h and functions reference_point.h holds a struct which will be used to set refrence point in the environment, plannet right now is ony to use the corners as such refrences. Also created a function to set theses refrence points inside the map_building.c file

---
+++ lib/reference_points.c
@@ -0,0 +1,7 @@
+/**
+ * File:          reference_points.h
+ * Date:          14.03.2014
+ * Description:   
+ * Author:        Stefan Klaus
+ * Modifications: V 0.1
+ */
---
+++ lib/reference_points.h
@@ -0,0 +1,26 @@
+/**
+ * File:          reference_points.h
+ * Date:          09.04.2014
+ * Description:   
+ * Author:        Stefan Klaus
+ * Modifications: V 0.1
+ */
+
+ struct referencePos {
+ 	struct {
+ 		float x;
+ 		float y;
+ 	}lower_left;
+ 	struct {
+ 		float x;
+ 		float y;
+ 	}lower_right;
+ 	struct {
+ 		float x;
+ 		float y;
+ 	}upper_left;
+ 	struct {
+		float x;
+ 		float y;
+ 	}upper_right;
+ };
